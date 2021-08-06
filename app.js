fe3c6233e27902b4148636b6393ef98825ab8ecb
seyi4mi <seyi4mi99@gmail.com>
2021-08-06 14:09:22 +0800

    Lesson 6b - Routing with Express

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.listen(3000); //we dont hv to write localhost coz it alrdy knws
 //responding to the request
 app.get("/", (req, res) => { //a get object with2 argu.  a)path/url  b)function dat takes in req n res obj
 
-    res.send("<p>Home page</p>"); //we dont hv to use the setheader obj it does it automatically
+    // res.send("<p>Home page</p>"); //we dont hv to use the setheader obj it does it automatically
 
-})
+    res.sendFile("./views/index.html", { root: __dirname });
+    //express will look 4 an absoulte path(root path) by default so, the ./view etc wont work
+    //we pass in another argu, that specify what the root is {__dirname} gets us the current directory we're in
+});
+
+
+app.get("/about", (req, res) => {
+    // res.send("<p>About page</p>");
+
+    res.sendFile("./views/about.html", { root: __dirname });
+});
